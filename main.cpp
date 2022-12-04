2db372e389823fc05d90c4cb6c8a5326f9afb606
LucasTruppel <lucasctruppelm@gmail.com>
2022-12-04 17:49:03 -0300

    codigo cria trie a partir de arquivo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "trie.h"
 
 int main() {
@@ -7,19 +8,37 @@ int main() {
 
     string filename;
     string word;
+    Trie trie;
 
     cin >> filename;  // entrada
 
-    cout << filename << endl;  // esta linha deve ser removida
+    ifstream file;
+    file.open(filename);
+    string line, dic_word;
+    int pos = 0;
 
-    Trie trie;
+    while (getline(file, line)) { // Leitura de cada linha do arquivo
+        bool writing_word = true;
+        char charc;
+        dic_word = "";
+        for (int i = 1; i < line.length(); i++) { // Leitura de cada caracter da linha
+            charc = line[i];
+            if (charc == ']')
+                break;
+            dic_word += charc;
+        }
+        trie.insert(dic_word, pos, line.length());
+        //cout << dic_word << " " << pos << " " << line.length() << endl;
+        pos += line.length() + 1;
+    }
     
     while (1) {  // leitura das palavras ate' encontrar "0"
         cin >> word;
         if (word.compare("0") == 0) {
             break;
         }
-        cout << word << endl;
+
+        //trie.find(word);
     }
 
     return 0;
